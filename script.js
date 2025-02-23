059f3bf37b0f85d010dff42c696d352f58528389
Rocky-jvzoo <rocky.t@myyahoo.com>
2025-02-23 16:14:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-
-const navId = document.getElementById("nav_menu"),
-  ToggleBtnId = document.getElementById("toggle_btn"),
-  CloseBtnId = document.getElementById("close_btn");
-
-// ==== SHOW MENU ==== //
-ToggleBtnId.addEventListener("click", () => {
-  navId.classList.add("show");
-});
-
-// ==== HIDE MENU ==== //
-CloseBtnId.addEventListener("click", () => {
-  navId.classList.remove("show");
-});
-
 // ==== Animate on Scroll Initialize  ==== //
 AOS.init();
 
@@ -24,21 +9,7 @@ gsap.from(".logo", {
   delay: 1,
   duration: 0.5,
 });
-// ==== NAV-MENU ==== //
-gsap.from(".nav_menu_list .nav_menu_item", {
-  opacity: 0,
-  y: -10,
-  delay: 1.4,
-  duration: 0.5,
-  stagger: 0.3,
-});
-// ==== TOGGLE BTN ==== //
-gsap.from(".toggle_btn", {
-  opacity: 0,
-  y: -10,
-  delay: 1.4,
-  duration: 0.5,
-});
+
 // ==== MAIN HEADING  ==== //
 gsap.from(".main-heading", {
   opacity: 0,
@@ -46,6 +17,7 @@ gsap.from(".main-heading", {
   delay: 2.4,
   duration: 1,
 });
+
 // ==== INFO TEXT ==== //
 gsap.from(".info-text", {
   opacity: 0,
@@ -53,6 +25,7 @@ gsap.from(".info-text", {
   delay: 2.8,
   duration: 1,
 });
+
 // ==== CTA BUTTONS ==== //
 gsap.from(".btn_wrapper", {
   opacity: 0,
@@ -60,6 +33,7 @@ gsap.from(".btn_wrapper", {
   delay: 2.8,
   duration: 1,
 });
+
 // ==== TEAM IMAGE ==== //
 gsap.from(".team_img_wrapper img", {
   opacity: 0,
